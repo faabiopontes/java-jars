f57eb7b4888a0af52c24743b77d6c850662608ba
Fabio Pontes <faabiopontes@gmail.com>
2021-07-18 21:36:29 -0300

    Java Jars (JJ): Lecture 3: Variables and Data Types

---
+++ java_basics/src/java_basics/Variables.java
@@ -0,0 +1,20 @@
+package java_basics;
+
+public class Variables {
+    public static void main(String[] args) throws Exception {
+        int x;
+        x = 34;
+
+        System.out.println(x);
+
+        x = 23;
+
+        System.out.println(x);
+
+        String words;
+
+        words = "this is a sentence";
+
+        System.out.println(words);
+    }
+}

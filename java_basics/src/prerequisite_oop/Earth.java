a6fcc5d11a276bbfa43f5a992da1ca6275eb950d
Fabio Pontes <faabiopontes@gmail.com>
2021-07-23 00:04:29 -0300

    JJ: Lecture 10: Program Flow

--- java_basics/src/prerequisite_oop/Earth.java
+++ java_basics/src/prerequisite_oop/Earth.java
@@ -2,11 +2,7 @@ package prerequisite_oop;
 
 public class Earth {
   public static void main(String[] args) {
-    Human fabio = new Human();
-    fabio.age = 29;
-    fabio.eyeColor = "brown";
-    fabio.heightInInches = 70;
-    fabio.name = "Fabio Pontes";
+    Human fabio = new Human("Fabio Pontes", 29, 70, "brown");
 
     fabio.speak();
   }

1e177b28ec4eb28573c9702d81926f1322eaba86
Fabio Pontes <faabiopontes@gmail.com>
2021-07-22 23:36:48 -0300

    JJ: Lecture 9: Class and Object

---
+++ java_basics/src/prerequisite_oop/Earth.java
@@ -0,0 +1,13 @@
+package prerequisite_oop;
+
+public class Earth {
+  public static void main(String[] args) {
+    Human fabio = new Human();
+    fabio.age = 29;
+    fabio.eyeColor = "brown";
+    fabio.heightInInches = 70;
+    fabio.name = "Fabio Pontes";
+
+    fabio.speak();
+  }
+}
